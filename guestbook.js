b44b97f3f673b0fe1688e397981383ebc4fb70b9
pavipixel <83488166+pavipixel@users.noreply.github.com>
2021-07-21 23:14:34 +0530

    Update guestbook.js
    Added API URL

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://eacead79.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
